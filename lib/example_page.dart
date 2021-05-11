068c93727afca8daf4ec7e57bdf75178649268f3
EduardoNatale <edunatalec@gmail.com>
2021-05-10 23:56:02 -0300

    example page

---
+++ lib/example_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ExamplePage extends StatelessWidget {
+  const ExamplePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
